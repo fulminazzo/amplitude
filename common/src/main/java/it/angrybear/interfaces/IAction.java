85d009ac2f8466378db52264e13c7a996f212870
Fulminazzo <fulminazzo21@gmail.com>
2024-01-15 19:52:02 +0100

    JavaDoc for IAction and implementations.

--- common/src/main/java/it/angrybear/interfaces/IAction.java
+++ common/src/main/java/it/angrybear/interfaces/IAction.java
@@ -4,9 +4,25 @@ import it.angrybear.interfaces.validators.OptionValidator;
 
 import java.util.Map;
 
+/**
+ * An interface used for identifying Minecraft events actions.
+ * See {@link it.angrybear.enums.ClickAction} and {@link it.angrybear.enums.HoverAction} for the implementations.
+ */
 public interface IAction {
 
+    /**
+     * Gets a map containing the required options.
+     * The parameters are given in a String-{@link OptionValidator}
+     * pair that allows to find and verify the validity of an option.
+     *
+     * @return the required options
+     */
     Map<String, OptionValidator> getRequiredOptions();
 
+    /**
+     * Gets the name.
+     *
+     * @return the name
+     */
     String name();
 }
