51f2a2b97af7683a7aab5e93a980d7829724f161
Fulminazzo <fulminazzo21@gmail.com>
2024-01-15 22:18:55 +0100

    JavaDoc for TextComponent

--- common/src/main/java/it/angrybear/components/TextComponent.java
+++ common/src/main/java/it/angrybear/components/TextComponent.java
@@ -16,6 +16,20 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The main component for working with messages.
+ * This component is able to read and intercept used tags throughout a raw string using {@link #setContent(String)}.
+ * Then, it parses them accordingly.
+ * <p>
+ * Supported values:
+ * <ul>
+ *     <li>&#60;<i>color</i>&#62; with colors taken from {@link Color}</li>
+ *     <li>&#60;<i>style</i>&#62; with styles taken from {@link Style}</li>
+ *     <li>&#60;hex&#62; which creates a new {@link HexComponent}</li>
+ *     <li>&#60;click&#62; which creates a new {@link ClickComponent}</li>
+ *     <li>&#60;hover&#62; which creates a new {@link HoverComponent}</li>
+ * </ul>
+ */
 @Getter
 public class TextComponent {
     public static final Map<String, Function<String, OptionComponent>> CONTAINER_COMPONENTS = new HashMap<>();
@@ -30,14 +44,27 @@ public class TextComponent {
     protected Boolean reset;
     protected @Nullable String text;
 
+    /**
+     * Instantiates a new Text component.
+     */
     public TextComponent() {
         this(null);
     }
 
+    /**
+     * Instantiates a new Text component.
+     *
+     * @param rawText the raw text
+     */
     public TextComponent(String rawText) {
         setContent(rawText);
     }
 
+    /**
+     * Sets content.
+     *
+     * @param rawText the raw text
+     */
     public void setContent(@Nullable String rawText) {
         if (CONTAINER_COMPONENTS.isEmpty()) {
             CONTAINER_COMPONENTS.put("click", ClickComponent::new);
@@ -100,10 +127,20 @@ public class TextComponent {
         setNext(rawText);
     }
 
+    /**
+     * Sets the next component (if {@link #isSimilar(TextComponent)} merge it with the current component).
+     *
+     * @param rawText the raw text
+     */
     public void setNext(String rawText) {
         setNext(new TextComponent(rawText));
     }
 
+    /**
+     * Sets the next component (if {@link #isSimilar(TextComponent)} merge it with the current component).
+     *
+     * @param next the next
+     */
     public void setNext(TextComponent next) {
         this.next = next;
 
@@ -116,6 +153,12 @@ public class TextComponent {
         }
     }
 
+    /**
+     * Checks the given component fields.
+     * For any field not given (null) set it to the value of this component.
+     *
+     * @param textComponent the text component
+     */
     public void setSameOptions(TextComponent textComponent) {
         TextComponent tmp = textComponent;
         while (tmp != null)
@@ -138,6 +181,9 @@ public class TextComponent {
             }
     }
 
+    /**
+     * Reset the formatting using {@link Style#RESET}.
+     */
     public void reset() {
         try {
             for (Field field : getOptions())
@@ -149,6 +195,11 @@ public class TextComponent {
         }
     }
 
+    /**
+     * Get all the options as a field array.
+     *
+     * @return the fields
+     */
     public Field @NotNull [] getOptions() {
         List<Field> fields = new ArrayList<>();
         Class<?> clazz = this.getClass();
@@ -165,6 +216,11 @@ public class TextComponent {
         return fields.toArray(new Field[0]);
     }
 
+    /**
+     * Get all the applied styles.
+     *
+     * @return the styles.
+     */
     public Style @NotNull [] getStyles() {
         return Arrays.stream(Style.values())
                 .filter(v -> {
@@ -179,88 +235,195 @@ public class TextComponent {
                 }).toArray(Style[]::new);
     }
 
+    /**
+     * Sets color.
+     *
+     * @param color the color
+     */
     public void setColor(Color color) {
         setColor(color, true);
     }
 
+    /**
+     * Sets color.
+     *
+     * @param color     the color
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setColor(Color color, boolean propagate) {
         this.color = color;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets magic.
+     *
+     * @return the magic
+     */
     public boolean getMagic() {
         return magic != null && magic;
     }
 
+    /**
+     * Sets magic.
+     *
+     * @param magic the magic
+     */
     public void setMagic(Boolean magic) {
         setMagic(magic, true);
     }
 
+    /**
+     * Sets magic.
+     *
+     * @param magic     the magic
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setMagic(Boolean magic, boolean propagate) {
         this.magic = magic;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets bold.
+     *
+     * @return the bold
+     */
     public boolean getBold() {
         return bold != null && bold;
     }
 
+    /**
+     * Sets bold.
+     *
+     * @param bold the bold
+     */
     public void setBold(Boolean bold) {
         setBold(bold, true);
     }
 
+    /**
+     * Sets bold.
+     *
+     * @param bold      the bold
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setBold(Boolean bold, boolean propagate) {
         this.bold = bold;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets strikethrough.
+     *
+     * @return the strikethrough
+     */
     public boolean getStrikethrough() {
         return strikethrough != null && strikethrough;
     }
 
+    /**
+     * Sets strikethrough.
+     *
+     * @param strikethrough the strikethrough
+     */
     public void setStrikethrough(Boolean strikethrough) {
         setStrikethrough(strikethrough, true);
     }
 
+    /**
+     * Sets strikethrough.
+     *
+     * @param strikethrough the strikethrough
+     * @param propagate     if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setStrikethrough(Boolean strikethrough, boolean propagate) {
         this.strikethrough = strikethrough;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets underline.
+     *
+     * @return the underline
+     */
     public boolean getUnderline() {
         return underline != null && underline;
     }
 
+    /**
+     * Sets underline.
+     *
+     * @param underline the underline
+     */
     public void setUnderline(Boolean underline) {
         setUnderline(underline, true);
     }
 
+    /**
+     * Sets underline.
+     *
+     * @param underline the underline
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setUnderline(Boolean underline, boolean propagate) {
         this.underline = underline;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets italic.
+     *
+     * @return the italic
+     */
     public boolean getItalic() {
         return italic != null && italic;
     }
 
+    /**
+     * Sets italic.
+     *
+     * @param italic the italic
+     */
     public void setItalic(Boolean italic) {
         setItalic(italic, true);
     }
 
+    /**
+     * Sets italic.
+     *
+     * @param italic    the italic
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setItalic(Boolean italic, boolean propagate) {
         this.italic = italic;
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets reset.
+     *
+     * @return the reset
+     */
     public boolean getReset() {
         return reset != null && reset;
     }
 
+    /**
+     * Sets reset.
+     *
+     * @param reset the reset
+     */
     public void setReset(Boolean reset) {
         setReset(reset, true);
     }
 
+    /**
+     * Sets reset.
+     *
+     * @param reset     the reset
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setReset(Boolean reset, boolean propagate) {
         this.reset = reset;
         color = Color.WHITE;
@@ -271,6 +434,12 @@ public class TextComponent {
         if (propagate) setSameOptions(next);
     }
 
+    /**
+     * Gets style.
+     *
+     * @param style the style
+     * @return the style
+     */
     public boolean getStyle(@Nullable Style style) {
         if (style == null) return false;
         String methodName = style.name();
@@ -284,10 +453,21 @@ public class TextComponent {
         }
     }
 
+    /**
+     * Sets style.
+     *
+     * @param style the style
+     */
     public void setStyle(Style style) {
         setStyle(style, true);
     }
 
+    /**
+     * Sets style.
+     *
+     * @param style     the style
+     * @param propagate if true, use {@link #setSameOptions(TextComponent)} to update the next component
+     */
     public void setStyle(@Nullable Style style, boolean propagate) {
         if (style == null) return;
         String methodName = style.name();
@@ -301,21 +481,13 @@ public class TextComponent {
         }
     }
 
-    @Override
-    public @NotNull String toString() {
-        String output = "{";
-        output += "next: " + next + ", ";
-        for (Field field : getOptions())
-            try {
-                Object object = field.get(this);
-                output += String.format("%s: %s, ", field.getName(), object);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        output += "text: " + text;
-        return output + "}";
-    }
-
+    /**
+     * Check if two text components are similar.
+     * "Similar" means if all their options (from {@link #getOptions()}) except {@link #text} are equal.
+     *
+     * @param textComponent the text component
+     * @return true if they are similar
+     */
     public boolean isSimilar(@Nullable TextComponent textComponent) {
         if (textComponent == null) return false;
         if (!this.getClass().equals(textComponent.getClass())) return false;
@@ -332,6 +504,13 @@ public class TextComponent {
         return true;
     }
 
+    /**
+     * Check if two text components are equal.
+     * Uses {@link #isSimilar(TextComponent)} and compares the raw texts.
+     *
+     * @param textComponent the text component
+     * @return true if they are equal
+     */
     public boolean equals(@NotNull TextComponent textComponent) {
         return isSimilar(textComponent) && Objects.equals(this.text, textComponent.text);
     }
@@ -340,5 +519,20 @@ public class TextComponent {
     public boolean equals(Object o) {
         if (o instanceof TextComponent) return equals((TextComponent) o);
         return super.equals(o);
+    }
+
+    @Override
+    public @NotNull String toString() {
+        String output = "{";
+        output += "next: " + next + ", ";
+        for (Field field : getOptions())
+            try {
+                Object object = field.get(this);
+                output += String.format("%s: %s, ", field.getName(), object);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        output += "text: " + text;
+        return output + "}";
     }
 }
