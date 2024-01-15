79d56106e4d64c9c8f3abd9d60626d5e076526ef
Fulminazzo <fulminazzo21@gmail.com>
2024-01-15 22:26:36 +0100

    JavaDoc for HexComponent

--- common/src/main/java/it/angrybear/components/HexComponent.java
+++ common/src/main/java/it/angrybear/components/HexComponent.java
@@ -6,12 +6,30 @@ import it.angrybear.interfaces.validators.OptionValidator;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represent a HEX component, an implementation of {@link TextComponent} that supports HEX colors.
+ * It requires "<b>color</b>" as an option, and it will be parsed using {@link HexColorValidator}.
+ * <p>
+ * Example: "<hex color=#FF00AA>Hello world!" will be parsed with:
+ * <ul>
+ *     <li>color: #ff00aa</li>
+ *     <li>text: Hello world!</li>
+ * </ul>
+ */
 public class HexComponent extends OptionComponent {
 
+    /**
+     * Instantiates a new Hex component.
+     */
     public HexComponent() {
         this(null);
     }
 
+    /**
+     * Instantiates a new Hex component.
+     *
+     * @param rawText the raw text
+     */
     public HexComponent(String rawText) {
         super(rawText);
     }
