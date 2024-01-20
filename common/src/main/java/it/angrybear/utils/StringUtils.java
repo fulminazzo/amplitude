80830b5f201fdeea40ba26bcc92d1c761dbc4418
Fulminazzo <fulminazzo21@gmail.com>
2024-01-20 14:46:40 +0100

    Created new splitQuoteSensitive method

--- common/src/main/java/it/angrybear/utils/StringUtils.java
+++ common/src/main/java/it/angrybear/utils/StringUtils.java
@@ -2,10 +2,43 @@ package it.angrybear.utils;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * String utilities.
  */
 public class StringUtils {
+
+    /**
+     * Split a string using the given separator, ignoring it if it is contained inside quotes.
+     *
+     * @param string    the string
+     * @param separator the separator
+     * @return the result of the split
+     */
+    public static @Nullable String[] splitQuoteSensitive(@Nullable String string, char separator) {
+        if (string == null) return null;
+        List<String> split = new ArrayList<>();
+
+        String tmp = "";
+        char startingQuote = 0;
+        for (char c : string.toCharArray()) {
+            if (startingQuote != 0) {
+                if (c == startingQuote) startingQuote = 0;
+            } else {
+                if (c == separator) {
+                    split.add(tmp);
+                    tmp = "";
+                    continue;
+                } else if (c == '"' | c == '\'') startingQuote = c;
+            }
+            tmp += c;
+        }
+        if (!tmp.isEmpty()) split.add(tmp);
+
+        return split.toArray(new String[0]);
+    }
 
     /**
      * A function that removes quotes from strings.
---
+++ common/src/test/java/it/angrybear/utils/StringUtilsTest.java
@@ -0,0 +1,18 @@
+package it.angrybear.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    void testSplitQuoteSensitive() {
+        String[] tmp = new String[]{"Hello", "\"world, are you ok?\"", "hope", "'you,are'"};
+        String rawString = "Hello,\"world, are you ok?\",hope,'you,are'";
+        assertEquals(Arrays.toString(tmp), Arrays.toString(StringUtils.splitQuoteSensitive(rawString, ',')));
+        assertArrayEquals(tmp, StringUtils.splitQuoteSensitive(rawString, ','));
+    }
+}
