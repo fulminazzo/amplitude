d2a55ad025a3a5c7a54296b59cbb2abaea9fbfcc
user <fulminazzo21@gmail.com>
2025-06-27 15:29:07 +0200

    Added RoseFluoComponent for testing purposes

---
+++ common/src/test/java/it/fulminazzo/amplitude/RoseFluoComponent.java
@@ -0,0 +1,24 @@
+package it.fulminazzo.amplitude;
+
+import it.fulminazzo.amplitude.component.Component;
+import it.fulminazzo.amplitude.component.CustomComponent;
+import it.fulminazzo.amplitude.component.HexComponent;
+
+public class RoseFluoComponent extends CustomComponent {
+
+    public RoseFluoComponent() {
+        super("rose");
+    }
+
+    public RoseFluoComponent(String rawText) {
+        super(rawText, "rose");
+    }
+
+    @Override
+    public Component toMinecraft() {
+        HexComponent component = new HexComponent("<hex color=#FF00AA>" + getText());
+        component.addNext(getNext());
+        return component;
+    }
+
+}
